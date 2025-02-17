4bc9dcabce4573993afcff12761bc2f89a198af6
oussemabenhassena5 (github) <oussemabenhassena5@gmail.com>
2025-02-17 16:32:20 +0100

    add mappings

---
+++ mappings.py
@@ -0,0 +1,27 @@
+SECTIONS = {
+    "m": "math",
+    "s": "sciences_exp",
+    "e": "economie",
+    "t": "technique",
+    "l": "lettres",
+    "p": "sport",
+    "i": "informatique",
+}
+
+SUBJECTS = {
+    "ma": "mathematiques",
+    "if": "informatique",
+    "te": "technique",
+    "sp": "sciences_physiques",
+    "sn": "sciences_naturelles",
+    "gs": "gestion",
+    "eg": "economiegestion",
+    "an": "anglais",
+    "fr": "francais",
+    "ph": "philosophie",
+    "al": "allemand",
+    "es": "espagnol",
+    "it": "italien",
+    "alg": "algorithmique",
+    "bd": "base_de_donnee",
+}
